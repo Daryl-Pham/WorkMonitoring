e33b80a4f25c8dd4a2b391a905256729683367b7
Khanh Pham <khanhpqworking@gmail.com>
2022-05-23 09:45:06 +0700

    - Config build MainLibrary include WMHook. - Update git ignore.

--- Client/WorksMonitoring/WMHook/WMHook.h
+++ Client/WorksMonitoring/WMHook/WMHook.h
@@ -6,21 +6,21 @@
 * @param hWnd Target of PostMessage().
 * @param keyboardMessage Target of message.
 */
-BOOL BthSetHookForKeyboard(HWND hWnd, UINT keyboardMessage);
+__declspec(dllexport) BOOL BthSetHookForKeyboard(HWND hWnd, UINT keyboardMessage);
 
 /**
 * Set hook for keyboard. You can get mouse event by catching mouseMessage.
 * @param hWnd Target of PostMessage().
 * @param mouseMessage Target of message.
 */
-BOOL BthSetHookForMouse(HWND hWnd, UINT mouseMessage);
+__declspec(dllexport) BOOL BthSetHookForMouse(HWND hWnd, UINT mouseMessage);
 
 /**
 * Unset keyboard event hook.
 */
-BOOL BthUnsetHookForKeyboard();
+__declspec(dllexport) BOOL BthUnsetHookForKeyboard();
 
 /**
 * Unset mouse event hook.
 */
-BOOL BthUnsetHookForMouse();
+__declspec(dllexport) BOOL BthUnsetHookForMouse();
